e175da226f4fd319101538c0111a06c5cf3aa46d
Jessica Yen-Yi Wu <jessicayywu@yahoo.com.tw>
2016-03-02 22:18:04 +0800

    Create Alertify
    ...

---
+++ alertify.js
@@ -0,0 +1,52 @@
+/*javascript: */
+if(document.getElementById("alertify") != null) {
+  document.getElementById("alertify").remove();
+}
+
+function deleteAlertify() {
+  document.getElementById("alertify").remove();
+}
+
+var newSection = document.createElement("section");
+newSection.id = "alertify";
+newSection.className = "alertify alertify-alert";
+
+var innerDiv = document.createElement("div");
+innerDiv.className = "alertify-dialog";
+
+var innerArticle = document.createElement("article");
+innerArticle.className = "alertify-inner";
+
+var innerP = document.createElement("p");
+innerP.className = "alertify-message";
+innerP.innerText = "...";
+
+var innerNav = document.createElement("nav");
+innerNav.className = "alertify-buttons";
+
+var innerBtn = document.createElement("button");
+innerBtn.className = "alertify-button alertify-button-ok";
+innerBtn.id = "alertify-ok";
+innerBtn.innerText = "OK";
+innerBtn.addEventListener( 'click' , deleteAlertify );
+
+innerNav.appendChild(innerBtn);
+innerArticle.appendChild(innerP);
+innerArticle.appendChild(innerNav);
+innerDiv.appendChild(innerArticle);
+newSection.appendChild(innerDiv);
+document.getElementsByTagName('body')[0].appendChild(newSection);
+
+/* The result will be like:
+<section id="alertify" class="alertify alertify-alert ">
+	<div class="alertify-dialog">
+		<article class="alertify-inner">
+			<p class="alertify-message">...</p>
+			<nav class="alertify-buttons">
+				<button class="alertify-button alertify-button-ok" id="alertify-ok">OK</button>
+			</nav>
+		</article>
+		<a id="alertify-resetFocus" class="alertify-resetFocus" href="#">Reset Focus</a>
+	</div>
+</section>
+*/
