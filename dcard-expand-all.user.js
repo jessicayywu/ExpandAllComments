f8afeb5894db31ad5228b250ad39624c0edcf9d5
Wei Ching Wang <andyw330@gmail.com>
2016-03-02 22:41:58 +0800

    Add userscript first version

---
+++ dcard-expand-all.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Dcard Expand All Comments
+// @version      0.1
+// @description  Expand all comments on Dcard
+// @author       White Hat & Who
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @match        https://www.dcard.tw/*
+// @downloadURL  
+// @updateURL    
+// @grant        None
+// ==/UserScript==
+
+function expandAllComments() {
+    var comments = document.getElementsByClassName("count")[1].innerText; /* 共 ... 則回應 */
+    var count = Math.floor(comments.split(' ')[1] / 50 - 2); /* 展開幾次 */
+    for (var i = 0; i < count; i++)
+        document.getElementById("expandBtn").click();
+}
+
+var expandAllBtn = document.createElement('button');
+expandAllBtn.setAttribute("class", "btn btn-default");
+expandAllBtn.textContent = "展開留言";
+expandAllBtn.addEventListener("click", expandAllComments, false);
+
+waitForKeyElements ('#expandBtn', addExpandAllButton);
+
+function addExpandAllButton(jNode){
+    $('.float-ops:first').prepend(expandAllBtn);
+}
