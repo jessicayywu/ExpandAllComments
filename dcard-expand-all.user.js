b3c89ade6d8151a6f525ff62aaa325758066a500
Wei Ching Wang <andyw330@gmail.com>
2016-03-06 01:17:31 +0800

    Add alertify to dcard-expand-all.user.js

--- dcard-expand-all.user.js
+++ dcard-expand-all.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dcard Expand All Comments
-// @version      0.1
+// @version      0.2
 // @description  Expand all comments on Dcard
 // @author       White Hat & Who
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -10,20 +10,78 @@
 // @grant        None
 // ==/UserScript==
 
-function expandAllComments() {
-    var comments = document.getElementsByClassName("count")[1].innerText; /* 共 ... 則回應 */
-    var count = Math.floor(comments.split(' ')[1] / 50 - 2); /* 展開幾次 */
-    for (var i = 0; i < count; i++)
-        document.getElementById("expandBtn").click();
-}
+var comments;
+var count;
 
+// create expandAll button element
 var expandAllBtn = document.createElement('button');
 expandAllBtn.setAttribute("class", "btn btn-default");
 expandAllBtn.textContent = "展開留言";
-expandAllBtn.addEventListener("click", expandAllComments, false);
+expandAllBtn.addEventListener("click", function() {
+    comments = document.getElementsByClassName("count")[1].innerText.split(' ')[1];  /* 共 {comments} 則回應 */
+    count = Math.round(comments / 50 - 2); /* 展開幾次 */
+    if (comments <= 500) {
+        expandAllComments();
+    } else {
+        showAlertify();
+    }
+}, false);
 
-waitForKeyElements ('#expandBtn', addExpandAllButton);
+// create alertify element
+var newSection = document.createElement("section");
+newSection.id = "expand-alertify";
+newSection.className = "alertify alertify-confirm";
 
-function addExpandAllButton(jNode){
+var innerDiv = document.createElement("div");
+innerDiv.className = "alertify-dialog";
+
+var innerArticle = document.createElement("article");
+innerArticle.className = "alertify-inner";
+
+var innerP = document.createElement("p");
+innerP.className = "alertify-message";
+innerP.innerText = "留言超過五百則，您確定要展開嗎？";
+
+var innerNav = document.createElement("nav");
+innerNav.className = "alertify-buttons";
+
+var innerCancelBtn = document.createElement("button");
+innerCancelBtn.className = "alertify-button alertify-button-cancel";
+innerCancelBtn.id = "alertify-cancel";
+innerCancelBtn.innerText = "Cancel";
+innerCancelBtn.addEventListener('click', deleteAlertify);
+
+var innerOKBtn = document.createElement("button");
+innerOKBtn.className = "alertify-button alertify-button-ok";
+innerOKBtn.id = "alertify-ok";
+innerOKBtn.innerText = "OK";
+innerOKBtn.addEventListener('click', function() {
+    expandAllComments();
+    deleteAlertify();
+});
+
+waitForKeyElements('#expandBtn', addExpandAllButton);
+
+function addExpandAllButton(jNode) {
     $('.float-ops:first').prepend(expandAllBtn);
+}
+
+function expandAllComments() {
+    for (var i = 0; i < count; i++)
+        document.getElementById("expandBtn").click();
+    $('.float-ops:first button:first').remove()
+}
+
+function showAlertify() {
+    innerNav.appendChild(innerCancelBtn);
+    innerNav.appendChild(innerOKBtn);
+    innerArticle.appendChild(innerP);
+    innerArticle.appendChild(innerNav);
+    innerDiv.appendChild(innerArticle);
+    newSection.appendChild(innerDiv);
+    document.getElementsByTagName('body')[0].appendChild(newSection);
+}
+
+function deleteAlertify() {
+    document.getElementById("expand-alertify").remove();
 }
